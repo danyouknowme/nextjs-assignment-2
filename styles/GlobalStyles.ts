eeae635030a583e67ff82c0a1afe616cd52e1408
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-03-04 22:11:59 +0700

    add font

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -5,10 +5,10 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: "Rubik", sans-serif;
   }
 
   body: {
-    font-family: "Rubik", sans-serif;
     overflow-x: hidden;
   }
 `;
