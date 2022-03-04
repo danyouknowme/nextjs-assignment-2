7d68a7a55f4b5ee392cfc7259bc8122ce53f2e42
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-03-05 01:55:15 +0700

    config images domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
+  images: {
+    domains: ['firebasestorage.googleapis.com']
+  },
+  compiler: {
     styledComponents: true,
   },
 };
