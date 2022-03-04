c73e38b6735c60b133c04f126899c5cfb7339093
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-03-05 01:55:36 +0700

    config slider settings

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -24,7 +24,9 @@ const Banner: React.FC = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000
   };
 
   return (
