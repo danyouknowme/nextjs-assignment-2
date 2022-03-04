802192a91ce24acc7ef38508a2262588baac0096
Thanathip <dannajaeiei@hotmail.com>
2022-03-04 17:27:06 +0700

    add homepage styling

---
+++ styles/Home.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+  padding: 0 2rem;
+`;
+
+export const HomeMain = styled.main`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
